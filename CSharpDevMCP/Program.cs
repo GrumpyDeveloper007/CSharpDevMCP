b10461df4db95dc334efa51a7a9fc3160b26ffb7
DalePitman <dale.pitman76@gmail.com>
2026-03-06 17:15:48 +1000

    Add POC

---
+++ CSharpDevMCP/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ModelContextProtocol.Protocol;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Debug;
+});
+builder.Services
+    .AddMcpServer(options =>
+    {
+        options.ServerInfo = new Implementation { Name = "TimeServer", Version = "1.0.0" };
+        //options.ListenAnyIP(5001, listenOptions => // Listen on port 5001 for HTTPS
+
+
+    })
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+var server = builder.Build();
+
+server.Run();
+
+
+Console.WriteLine("Hello MCP World!");
+
